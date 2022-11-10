90aa22f1ac59e13b3a8cda232371ca56df2fab37
TC1085 <ika.tania.ft21@mail.umy.ac.id>
2022-11-10 17:11:27 +0700

    make class list and add method for add node to list

--- singly_linked_list/Program.cs
+++ singly_linked_list/Program.cs
@@ -12,6 +12,25 @@ namespace singly_linked_list
         public string nama;
         public Node next;
     }
+    class List
+    {
+        Node START;
+        public List()
+        {
+            START = null;
+        }
+        //Method untuk menambahkan sebuah node ke dalam list
+
+        public void addNote()
+        {
+            int nim;
+            string nm;
+            Console.WriteLine("\nMasukan nomer Mahasiswa");
+            nim = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\nMasukan nama Mahasiswa");
+            nm = Console.ReadLine();
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
